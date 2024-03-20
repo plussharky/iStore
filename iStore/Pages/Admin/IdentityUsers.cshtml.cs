fcc6c34fd0d0f197139935158d5228ae876aa73c
Plusharky <Plusharky@PLUSHARKY-PC>
2024-03-20 23:56:18 +0300

    Added a conventional administration feature

---
+++ iStore/Pages/Admin/IdentityUsers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace iStore.Pages.Admin
+{
+    public class IdentityUsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
